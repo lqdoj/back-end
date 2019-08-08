a21483f4e37a588cf26f14ff052c9218e8df2b54
Nguyen Le Ly Bang <bangjdev@gmail.com>
2019-08-08 19:01:27 +0700

    Built register module

--- users/views.py
+++ users/views.py
@@ -1,23 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 import json
 
 
-data = {
-	'username' : '',
-	'password' : '',
-	'email': ''
-}
+def create_message(status, message):
+	data = {
+		'status' : status,
+		'message' : message
+	}
+
+	return json.dumps(data)
 
 
-# Create your views here.
+def exist_user(username):
+	return User.objects.filter(username=username).exists()
+
+def create_user(username, password, email):
+	return User.objects.create_user(username=username, email=email, password=password)
+
+
 @api_view(["POST"])
 def register(request):
-	# username = request.body
-	print(request.body.decode('utf-8'))
-	data = {
-		'params' : request.body.decode('utf-8')
-	}
-	return Response(data, status=200)
+
+	# parse request params
+
+	data = json.loads(request.body)['data']
+	username = data['user_name']
+	password = data['password']
+	email = data['email']
+
+	# check user existence
+
+	if exist_user(username):
+		return Response(data=create_message("fail", "User existed"), status=200)
+
+	# create user
+	try:
+		user = create_user(username, password, email)
+	except error:
+		return Response(data=create_message("fail", "Error: " + error), status=200)
+	
+	return Response(data=create_message("success", "User created"), status=200)
